b1cbaec276a8ff4654e4a3194b7dc7badcfeb7b4
KevinComba <kelchospense88@gmail.com>
2024-09-03 10:43:18 +0300

    added daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
+      <button className="btn btn-accent">Accent</button>
     </h1>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
   content: [
     "./index.html",
@@ -7,6 +8,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui,],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }
 
