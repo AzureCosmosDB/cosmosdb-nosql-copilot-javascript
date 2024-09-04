eae7e84b7c182fa8a697eedf86bbe86d9ff76b80
KevinComba <kelchospense88@gmail.com>
2024-09-04 10:37:44 +0300

    multi theme functionality added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,61 @@
+import { Bot } from "lucide-react";
+import { useMsal } from "@azure/msal-react";
+import UserProfile from '../components/UserProfile'
+import ThemeChanger from './ThemeChanger';
+
+interface IUserProfile {
+    authorityType: string;
+    name: string;
+    username: string;
+}
+
+
+
+export default function Navbar() {
+    const { accounts, instance } = useMsal();
+
+    const userData: IUserProfile = {
+        authorityType: accounts[0]?.authorityType ?? '',
+        name: accounts[0]?.name ?? '',
+        username: accounts[0]?.username ?? '',
+    };
+    const openModal = () => {
+        const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
+        modal.showModal();
+    };
+
+    return (
+        <>
+            <div className="navbar bg-base-100 shadow-xl">
+                <div className="flex-1">
+                    <span className="btn btn-ghost text-xl">
+                        <Bot className="hover:animate-spin hover:text-red-600" />
+                        ChatGPT Assistant
+                    </span>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+
+                        <li>
+                            <details>
+                                <summary>{userData.name}</summary>
+                                <ul className="bg-base-100 rounded-t-none p-2">
+                                    <li>
+                                        <button onClick={openModal}>
+                                            Profile
+                                        </button>
+                                    </li>
+                                    <li><button onClick={() => instance.logoutPopup()}>Logout</button></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <ThemeChanger />
+                    </ul>
+                </div>
+            </div>
+            <UserProfile userData={userData} />
+        </>
+    );
+}
+
+
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,36 +1,27 @@
 import { useIsAuthenticated } from "@azure/msal-react";
 import { useNavigate } from "react-router-dom";
-import { useMsal } from "@azure/msal-react";
+import Navbar from "../components/Navbar";
+import { useEffect } from "react";
 
 
 export default function HomePage() {
 
   const isAuthenticated = useIsAuthenticated();
   const navigate = useNavigate();
-  const { accounts, instance } = useMsal();
-  const userData = accounts[0]!;
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    }
+
+  }, [isAuthenticated, navigate]);
 
 
-  if (!isAuthenticated) {
-    navigate("/");
-  } else {
-    return (
-      <div className="card bg-base-100 w-96 shadow-xl min-h-fit">
-        <div className="card-body">
-          <h2 className="card-title">
-            AuthType:
-            <div className="badge badge-secondary">{userData.authorityType} </div>
-          </h2>
-          <p>Name:{userData.name}</p>
-          <p>Email:{userData.username}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-sm btn-warning"
-              onClick={() => instance.logoutPopup()}>
-              LogOut
-            </button>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  return (
+    <>
+      <Navbar />
+      <h3>chatPage</h3>
+
+    </>
+  )
 }
