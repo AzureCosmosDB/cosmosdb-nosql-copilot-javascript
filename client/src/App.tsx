2de18dbb5679af2c10a1c19106c9129b0f7e74df
KevinComba <kelchospense88@gmail.com>
2024-09-09 01:21:48 +0300

    shadcn button test done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <Button>Click me</Button>
     </h1>
   )
 }
