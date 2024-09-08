01648839031978c512bab11008c0cee91eb7d656
KevinComba <kelchospense88@gmail.com>
2024-09-09 01:27:07 +0300

    added toaster globally

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Toaster } from '../src/components/ui/sonner.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 )
