3b113f52bd12ce9179d68af1de35de33a386f932
KevinComba <kelchospense88@gmail.com>
2024-09-09 01:18:37 +0300

    configured shadcn ui and vite-plugin-mkcert

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import mkcert from 'vite-plugin-mkcert'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
